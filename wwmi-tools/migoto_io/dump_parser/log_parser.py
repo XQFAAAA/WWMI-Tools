2bcb556d239ba722c010cdfcc6d6730d58256eca
xqfa <xqfaaaa@gmail.com>
2026-05-14 22:26:44 +0800

    将原有的逐行状态跟踪解析改为先按call_id分组再批量处理

--- wwmi-tools/migoto_io/dump_parser/log_parser.py
+++ wwmi-tools/migoto_io/dump_parser/log_parser.py
@@ -52,7 +52,7 @@ class FrameDumpCall:
 
 class FrameDumpLog:
     def __init__(self, dump_path):
-        self.path = os.path.join(dump_path, 'log.txt')
+        self.path = os.path.join(dump_path, 'log.txt') # 拼接log.txt路径
         self.calls = {}
         self.parse_log()
         self.validate()
@@ -61,31 +61,56 @@ class FrameDumpLog:
         pass
 
     def parse_log(self):
-        self.calls = {}
-        with (open(self.path, "r") as f):
+        with open(self.path, "r") as f:
             lines = f.readlines()
-            call = None
-            raw_log_entry = []
-            for line_id, line in enumerate(lines):
-                raw_call_id = line[0:6]
-                if raw_call_id.isnumeric():
-                    line_call_id = int(raw_call_id)
-                    if call is None:
-                        call = FrameDumpCall(line_call_id)
-                        self.calls[raw_call_id] = call
-                    elif line_call_id != call.id:
-                        call = FrameDumpCall(line_call_id)
-                        if line_call_id in self.calls:
-                            raise ValueError(f'Malformed log line {line_id}: '
-                                             f'data collection for call id {raw_call_id} was already finished, '
-                                             f'current call id: {call.id}')
-                        self.calls[raw_call_id] = call
-                    call.import_data(raw_log_entry)
-                    raw_log_entry = [line[7:]]
-                elif call is None:
-                    continue
-                else:
-                    raw_log_entry.append(line.strip())
-            # Handle last line of the log
+
+        # 按call_id分组
+        grouped = []
+        for line in lines:
+            raw_call_id = line[0:6]
+            if raw_call_id.isnumeric():
+                grouped.append((raw_call_id, [line[7:]]))
+            elif grouped: # 非数字开头，且不是第一行analyse_options
+                grouped[-1][1].append(line.strip())
+
+        call = None
+        for raw_call_id, raw_log_entry in grouped:
+            call_id = int(raw_call_id)
+            # 首个或变更时，创建新的call
+            if call is None or call_id != call.id:
+                if call_id in self.calls:
+                    raise ValueError(
+                        f'Call id {raw_call_id} was already finished, '
+                        f'current call id: {call_id}'
+                    )
+                call = FrameDumpCall(call_id)
+                self.calls[raw_call_id] = call
+            # 正则匹配
             call.import_data(raw_log_entry)
+
+        # for line_id, line in enumerate(lines):
+        #     raw_call_id = line[0:6]
+        #     if raw_call_id.isnumeric():
+        #         line_call_id = int(raw_call_id)
+
+        #         if call is None: # 第二行000001，直接创建call
+        #             call = FrameDumpCall(line_call_id)
+        #             self.calls[raw_call_id] = call
+                
+        #         if line_call_id != call.id: # 新的call，如000001->000002
+        #             call = FrameDumpCall(line_call_id)
+        #             if line_call_id in self.calls:
+        #                 raise ValueError(f'Malformed log line {line_id}: '
+        #                                     f'data collection for call id {raw_call_id} was already finished, '
+        #                                     f'current call id: {call.id}')
+        #             self.calls[raw_call_id] = call
+                
+        #         call.import_data(raw_log_entry)
+        #         raw_log_entry = [line[7:]]
+        #     elif call is None: # 第一行analyse_options，直接跳过
+        #         continue
+        #     else:
+        #         raw_log_entry.append(line.strip())
+        # # Handle last line of the log
+        # call.import_data(raw_log_entry)
 
